3081e7796afd81e8f24331bc01d6ec95b5ed9411
qkrtnrlf <dkslel217@naver.com>
2022-05-23 19:58:29 +0900

    [박수길] init pr test

---
+++ marketback/src/main/java/com/example/marketback/controller/chatting/controllerTest.java
@@ -0,0 +1,9 @@
+package com.example.marketback.controller.chatting;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@RestController
+public class controllerTest {
+}
