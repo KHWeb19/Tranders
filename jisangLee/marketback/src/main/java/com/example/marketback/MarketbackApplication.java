68997315842261121e652e468d6ef0c36cf041fd
potential007 <potential007@naver.com>
2022-05-23 20:25:33 +0900

    [이지상] init pr test

---
+++ jisangLee/marketback/src/main/java/com/example/marketback/MarketbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketbackApplication.class, args);
+	}
+
+}
