533dcdbc293491b413e7f261f6df9277f02005d1
sinareum <kuuniin@gmail.com>
2022-05-24 21:49:43 +0900

    [신아름] Init project repo

--- marketfront/src/views/HomePage.vue
+++ marketfront/src/views/HomePage.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <HelloWorld/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomePage',
   components: {
-    HelloWorld
+
   }
 }
 </script>
