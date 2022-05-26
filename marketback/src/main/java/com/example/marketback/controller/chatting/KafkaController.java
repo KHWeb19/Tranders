b11dbd9f4fc17f72718ddea30444f77925a7208f
ljieun <wldms8196@gmail.com>
2022-05-26 22:48:50 +0900

    [이지은] feat: kafka spring 연동

---
+++ marketback/src/main/java/com/example/marketback/controller/chatting/KafkaController.java
@@ -0,0 +1,26 @@
+package com.example.marketback.controller.chatting;
+
+import com.example.marketback.service.chatting.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping
+    public String sendMessage(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+
+        return "success";
+    }
+}
---
+++ marketback/src/main/java/com/example/marketback/service/test.java
@@ -0,0 +1,4 @@
+package com.example.marketback.service;
+
+public class test {
+}
