cccdcda38c1fcc9d80666de76033f8684885f1f7
sinareum <kuuniin@gmail.com>
2022-05-28 21:42:35 +0900

    [신아름] feat: Register page ui [RP]

---
+++ marketfront/src/views/member/OrdinaryRegisterPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ordinary-register-view @register="register"></ordinary-register-view>
+  </div>
+</template>
+
+<script>
+import OrdinaryRegisterView from "@/components/member/OrdinaryRegisterView";
+import axios from "axios";
+
+export default {
+  name: "OrdinaryRegisterPage",
+  components: {OrdinaryRegisterView},
+  methods: {
+    register(payload){
+      const {id, pw, name, phoneNum, region} = payload;
+
+      axios.post('http://localhost:7777/member/register', {id, pw, name, phoneNum, region})
+        .catch((res) => {
+          alert(res)
+        })
+        .then((res) => {
+          console.log(res)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
