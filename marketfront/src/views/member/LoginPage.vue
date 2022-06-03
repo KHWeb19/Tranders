5464b10f0d45ce5f3ca02e36d2910837c859fed3
sinareum <kuuniin@gmail.com>
2022-06-03 21:30:44 +0900

    [신아름] feat: LoginPage UI [LP]

---
+++ marketfront/src/views/member/LoginPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <login-view @login="login"></login-view>
+  </div>
+</template>
+
+<script>
+import LoginView from "@/components/member/LoginView";
+import axios from "axios";
+export default {
+  name: "LoginPage",
+  components: {LoginView},
+  methods: {
+    login(payload){
+      const {id, password} = payload;
+
+      axios.post("http://localhost:7777/login", {id, password})
+        .catch((res) => {
+          alert(res + "에러 발생")
+        })
+        .then((res) => {
+          console.log(res)
+          console.log(res.headers.Authorization)
+          console.log(res.headers)
+          console.log(res.status)
+          console.log(res.headers['Authorization'])
+          this.$router.push({name: "HomePage"})
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
