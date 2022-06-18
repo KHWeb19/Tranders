2ca6198b631b56515263a5ab892c84ad95c07a19
ljieun <wldms8196@gmail.com>
2022-06-18 22:30:15 +0900

    [이지은] feat: 채팅방과 멤버 조인 구현 중 [CMS]

--- marketback/src/main/java/com/example/marketback/entity/chatting/ChatRoom.java
+++ marketback/src/main/java/com/example/marketback/entity/chatting/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.example.marketback.entity.chatting;
 
+import com.example.marketback.entity.member.Member;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,8 +20,17 @@ public class ChatRoom {
     @Column(name="room_no")
     private Long roomNo;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name= "member_no")
+    private Member member1;
+
+    //member와 조인
+//    @Column(length = 32, nullable = false)
+//    private String member1;
+
+    //member와 조인
     @Column(length = 32)
-    private String sender;
+    private String member2;
 
     @Column(length = 32)
     private String appointDate;
